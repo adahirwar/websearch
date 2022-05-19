4ef15a6d539019eab3c384f2138a26a5523468f6
arvind ahirwar <ad.ahirwar@gmail.com>
2022-05-19 13:36:18 +0530

    Update __init__.py

--- websearch/__init__.py
+++ websearch/__init__.py
@@ -3,28 +3,51 @@
 """
 Created on Thu May 19 09:13:20 2022
 
-@author: indus
+@author: Arvind Ahirwar
 """
-from bs4 import BeautifulSoup
-from requests import get
+import csv
+import json
+import os
+import re
+import unicodedata
+from collections import deque
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+from time import sleep
 
-usr_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+import click
+import requests
+from lxml import html
 
-def _req(term, results, lang, start, proxies):
-    resp = get(
-        url="https://duckduckgo.com/",
-        headers=usr_agent,
-        params=dict(
-            q = term,
-            num = results + 2, # Prevents multiple requests
-            hl = lang,
-            start = start,
-        ),
-        proxies=proxies,
-    )
-    resp.raise_for_status()
-    return resp
+__version__ = "1.0.1"
 
+
+session = requests.Session()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"
+}
+session.headers.update(headers)
+
+
+def _slugify(filename):
+    """
+    Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+
+    filename = unicodedata.normalize("NFC", filename)
+    filename = re.sub(r"[^\w\s-]", "", filename.lower())
+    return re.sub(r"[-\s]+", "-", filename).strip("-_")
+
+
+def _normalize(text):
+    if text:
+        body = html.fromstring(text)
+        return html.tostring(body, method="text", encoding="unicode")
+    
 class SearchResult:
     def __init__(self, url, title, description):
         self.url = url
@@ -34,37 +57,71 @@ class SearchResult:
     def __repr__(self):
         return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
 
-def search(term, num_results=10, lang="en", proxy=None, advanced=False):
-    escaped_term = term.replace(' ', '+')
 
-    # Proxy
-    proxies = None
-    if proxy:
-        if proxy[:5]=="https":
-            proxies = {"https": proxy} 
-        else:
-            proxies = {"http": proxy}
-    
-    # Fetch
-    start = 0
-    while start < num_results:
-        # Send request
-        resp = _req(escaped_term, num_results-start, lang, start, proxies)
+def search(
+    keywords,
+    region="wt-wt",
+    safesearch="Moderate",
+    time=None,
+    max_results=5,
+    output=None,
+):
+    """DuckDuckGo search
+    Query parameters, link: https://duckduckgo.com/params:
+    keywords: keywords for query;
+    safesearch: On (kp = 1), Moderate (kp = -1), Off (kp = -2);
+    region: country of results - wt-wt (Global), us-en, uk-en, ru-ru, etc.;
+    time: 'd' (day), 'w' (week), 'm' (month), 'y' (year);
+    max_results = 5 gives a number of results not less than 5,
+                  maximum Duckduckgo gives out about 200 results,
+    output: csv, json, print.
+    """
 
-        # Parse
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        result_block = soup.find_all('div', attrs={'class': 'serp__results'})
-        for result in result_block:
-            # Find link, title, description
-            link = result.find('a', href=True)
-            title = result.find('h3')
-            description_box = result.find('div', {'style': '-webkit-line-clamp:2'})
-            if description_box:
-                description = description_box.find('span')
-                if link and title and description:
-                    start += 1
-                    if advanced:
-                        yield SearchResult(link['href'], title.text, description.text)
-                    else:
-                        yield link['href']
+    if not keywords:
+        return None
+
+    # get vqd
+    payload = {
+        "q": keywords,
+    }
+    res = session.post("https://duckduckgo.com", data=payload)
+    tree = html.fromstring(res.text)
+    vqd = (
+        tree.xpath("//script[contains(text(), 'vqd=')]/text()")[0]
+        .split("vqd='")[-1]
+        .split("';")[0]
+    )
+    sleep(0.75)
+
+    # search
+    safesearch_base = {"On": 1, "Moderate": -1, "Off": -2}
+    params = {
+        "q": keywords,
+        "l": region,
+        "p": safesearch_base[safesearch],
+        "s": 0,
+        "df": time,
+        "o": "json",
+        "vqd": vqd,
+    }
+    results = []
+    while len(results) < max_results and params["s"] < 200:
+        resp = session.get("https://links.duckduckgo.com/d.js", params=params)
+        try:
+            data = resp.json()["results"]
+        except:
+            return results
+
+        for r in data:
+            try:
+                s = r["n"].split("s=")[1].split("&")[0]
+                params["s"] = int(s) - int(s) % 2
+                break
+            except:
+                    title = _normalize(r["t"])
+                    href = r["u"]
+                    body = _normalize(r["a"])
+                    if body:
+                        sleep(0.75)
+                        yield SearchResult(href, title, body)
                         
